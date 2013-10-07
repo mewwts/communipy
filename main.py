f075331bd0f5d8e81d261ddf73b0aaedf921cf65
Mats Julian Olsen <matsjuli@markov.math.ntnu.no>
2013-10-07 21:30:17 +0200

    minor edit to main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def main():
     #filename = '../Data/karate.gml'
     #filename = '../Data/lesmis.gml'
     #filename = '../Data/as-22july06.gml'
-    filename = '../Data/polbooks.gml'
-
+    #filename = '../Data/polbooks.gml'
+    filename = "../Data/MAT_MIN_20111221.mat" 
     #A = sparse.csr_matrix(np.genfromtxt(filename, delimiter=',')) # from the articel
-    #A = sparse.csr_matrix(io.loadmat("../Data/MAT_MIN_20111221.mat")['mat'])
-    A = nx.to_scipy_sparse_matrix(nx.read_gml(filename)) #Karate
+    A = sparse.csr_matrix(io.loadmat(filename)['mat'])
+    #A = nx.to_scipy_sparse_matrix(nx.read_gml(filename)) #Karate
     n = A.shape[1]
     k = np.array(A.sum(1)).reshape(n) # the degree sequence
     m = 0.5*A.sum() # the number of edges
