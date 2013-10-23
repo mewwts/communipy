a44d2c2ca5ff881105cd50e124c773c3fbf16f0e
mewwts <mats@plysjbyen.net>
2013-10-23 22:48:51 +0200

    Nothing new here

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def main():
     #filename = '../Data/example.csv'
     #filename = '../Data/karate.gml'
     #filename = '../Data/lesmis.gml'
-    filename = '../Data/as-22july06.gml'
+    #filename = '../Data/as-22july06.gml'
     #filename = '../Data/polbooks.gml'
-    #filename = "../Data/MAT_MIN_20111221.mat" 
+    filename = "../Data/MAT_MIN_20111221.mat" 
     #A = sparse.csr_matrix(np.genfromtxt(filename, delimiter=',')) # from the articel
-    #A = sparse.csr_matrix(io.loadmat(filename)['mat'])
-    A = nx.to_scipy_sparse_matrix(nx.read_gml(filename)) #gmls
+    A = sparse.csr_matrix(io.loadmat(filename)['mat'])
+    #A = nx.to_scipy_sparse_matrix(nx.read_gml(filename)) #gmls
     n = A.shape[1]
-    k = list(sum(A).data)#list(reduce(add, A).data)
+    k = list(reduce(add, A).data) # list(sum(A).data)#
     #k = np.array(A.sum(1)).reshape(n) # the degree sequence
     m = 0.5*A.sum() # the number of edges
     filewriter = Matwriter(filename)
