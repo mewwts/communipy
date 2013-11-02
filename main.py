323ded48515f99215e7ad4fd66e3c5b00c191f13
mewwts <mats@plysjbyen.net>
2013-11-02 18:24:12 +0100

    Cleanup

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def initialize(filepath, args):
     tsh = args.treshold if args.treshold else 0.02
     verbose = args.verbose if args.verbose else False
     dump = args.dump if args.dump else False
-    
+
     t = time.time()
     louvain.louvain(A, m, n, k, filewriter, tsh, verbose, dump)
     print "It took %s seconds" % (time.time() - t)
@@ -50,19 +50,8 @@ if __name__ == '__main__':
     parser.add_argument("-o", "--output", help="Output too file in ./results/", action="store_true")
     parser.add_argument("-d", "--dump", help="Dump communities", action="store_true")
     args = parser.parse_args()
+
     if os.path.isfile(args.path_to_file):
             initialize(args.path_to_file, args)
     else:
         print "Please provide a valid file"
-
-    #filename = '../Data/example.csv'
-    #filename = '../Data/karate.gml'
-    #filename = '../Data/lesmis.gml'
-    #filename = '../Data/netscience.gml'
-    #filename = '../Data/internet.dat.gz'
-    #filename = '../Data/cond-mat-2003.gml'
-    #filename = '../Data/dolphins.mat'
-    #filename = '../Data/as-22july06.gml'
-    #filename = '../Data/polbooks.gml'
-    #filename = "../Data/MAT_MIN_20111221_sym.mat" 
-    #filename = "../Data/MAT_MIN_sym.mat" 
