ea4ea3d8d9f6c305886e6f213df875431a325bed
mewwts <mats@plysjbyen.net>
2014-03-05 14:50:22 +0100

    Getters and Setters are so 90s

--- labels.py
+++ labels.py
@@ -4,50 +4,23 @@ class Labels(Communities):
 
     def __init__(self, iterable, k):
         super(Labels, self).__init__(iterable, k)
-        self._internal = k[:]
-        self._d = [0.0] * len(self._nodes)
-        self._p = [1.0/len(self._nodes)] * len(self._nodes)
+        self.internal = k[:]
+        self.d = [0.0] * len(self._nodes)
+        self.p = [1.0/len(self._nodes)] * len(self._nodes)
 
     def move(self, i, s, k_i, intra=None):
         super(Labels, self).move(i, s, k_i)
         if intra is not None:
             try:
-                self._internal[i] = intra
+                self.internal[i] = intra
             except IndexError:
                 'Warning, IndexError: No such vertex in list.'
 
-    def get_internal(self, i):
-        try:
-            a = self._internal[i]
-        except IndexError:
-            'Warning, IndexError: No such vertex in list.'
-        else:
-            return a
-
-    def set_d(self, i, di):
-        try:
-            self._d[i] = di
-        except IndexError:
-            print 'Warning, IndexError: No such vertex in list.'
-    
-    def get_d(self, i):
-        try:
-            d = self._d[i]
-        except IndexError:
-            print 'Warning, IndexError: No such vertex in list.'
-        else:
-            return d
-
-    def set_p(self, i, pi):
-        try:
-            self._p[i] = pi
-        except IndexError:
-            print 'Warning, IndexError: No such vertex in list.'
-    
-    def get_p(self, i):
-        try:
-            p = self._p[i]
-        except IndexError:
-            print 'Warning, IndexError: No such vertex in list.'
-        else:
-            return p
+    def recluster(self, com_dict, k):
+        super(Labels, self).recluster(com_dict, k)
+        # Should avoid this iteration
+        for name, coms in com_dict.iteritems():
+            for c_i in coms[1:]:
+                for i in self.get_nodes(c_i):
+                    # WILD approximation
+                    self._internal[i] = k - self._internal[i]
