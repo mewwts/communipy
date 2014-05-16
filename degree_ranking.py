5d2024a5200a4a10c4217ad6c4b175207224eae6
mewwts <mats@plysjbyen.net>
2014-05-16 15:18:03 +0200

    the next vertex we try to establish a community around has not been moved this pass

--- degree_ranking.py
+++ degree_ranking.py
@@ -1,9 +1,4 @@
-from operator import itemgetter
 from collections import deque
-from communities import Communities
-from labels import Labels
-from communities import Communities
-import numpy as np
 from modularity import get_gain
 from utils import rank
 
@@ -18,16 +13,14 @@ def degree_rank(G, C, q, arguments):
     while True:
         new_q = degree_rank_inner(G, C, knbs, 
             consider, not_seen, q, arguments)
-        not_seen = set(xrange(n))
-        consider = rank([k[i] for i in 
-            {node for j in not_seen for node in knbs[j]}])
-
+        # not_seen = set(xrange(n))
+        # consider = rank([k[i] for i in 
+        #     {node for j in not_seen for node in knbs[j]}])        
         if new_q - q < arguments.tsh:
             break
-        else:
-            q = new_q
+        q = new_q
 
-    return q
+    return new_q
 
 def degree_rank_inner(G, C, knbs, consider, not_seen, old_q, args):
     """
@@ -47,10 +40,7 @@ def degree_rank_inner(G, C, knbs, consider, not_seen, old_q, args):
     q: modularity
 
     """
-    A = G.A
-    k = G.k
-    m = G.m
-    n = G.n
+    A, m, n, k = G
     q = old_q
     moved = set([])
     index = 0
@@ -64,23 +54,24 @@ def degree_rank_inner(G, C, knbs, consider, not_seen, old_q, args):
         for j in nbs:
             if j in moved:
                 continue
-            not_seen.discard(j)
-            knbs[j].add(i)
-            data = A.data[A.indptr[j]:A.indptr[j+1]]
-            indices = A.indices[A.indptr[j]:A.indptr[j+1]]
+            not_seen.discard(j) # We've seen j
+            knbs[j].add(i) # i is a high-degree nb of j
             if C.nodes[i] != C.nodes[j]:
                 movein, moveout = get_gain(G, C, j, C.nodes[i])
                 if movein + moveout > 0:
                     moved.add(j)
                     C.move(j, C.nodes[i], k[j])
-                    q += movein - moveout
+                    q += movein + moveout
 
         if not_seen:
-            index += 1
             try:
-                next = consider[index] # should next not be moved?
+                while True:
+                    index += 1
+                    next = consider[index]
+                    if next not in moved:
+                        break
             except IndexError:
                 return q
-            else:
-                queue.append(next)
-    return q
+            queue.append(next)
+        else:
+            return q
