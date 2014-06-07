5683ffe8973ca895b4c39f049608ffce51c1077a
mewwts <mats@plysjbyen.net>
2014-06-07 21:15:25 +0200

    Labelprop has been completely rewritten in the style of the original (shitty) java code.

--- labelprop.py
+++ labelprop.py
@@ -1,196 +1,209 @@
-import copy
-from labels import Labels
-from community_detection import community_network
-import modularity
-import functions as fns
-import numpy as np
-import sys
+from __future__ import division
+from operator import itemgetter
 from collections import defaultdict
+import functions as fns
+import modularity
+import copy
+import numpy as np
+from labels import Labels
 from utils import Graph
-import numexpr as nr
+from community_detection import community_network
+MAX_ITER = 100
 
 def propagate(G, args):
-    A, m, n, k = G
-    C = dpa(G, args)   
-    coms = C.dict_renamed
-    new_mat = community_network(A, coms)
-    new_k = np.array(new_mat.sum(axis=1), 
-                     dtype=float).reshape(-1,).tolist()    
-
+    G.A.data = np.repeat(1, G.A.data.shape[0])
+    k = np.array(G.A.sum(axis=1), dtype=float).reshape(-1,).tolist()
+    G = Graph(G.A, len(G.A.data) / 2, G.n, k)
+    C = dpa(G, args)
     print("Found {} communities".format(len(C.dict_renamed)))
-    print("Modularity of {}".format(
-                modularity.diagonal_modularity(
-                    new_mat.diagonal(), new_k, 0.5*new_mat.sum())))
+    print("Modularity = {}".format(modularity.modularity(G.A, G.k, G.m, C)))
     if args.exporter:
         args.exporter.write_nodelist(C.dict_renamed)
         args.exporter.close()
         print('Community structure outputted to .txt-file')
 
-def dpa(G, args):
-    """ Diffusion and propagation algorithm """
-    ddC = Labels(xrange(G.n), G.k)
-    dalpa(G, ddC, False)
-    ddQ = modularity.modularity(G.A, G.k, G.m, ddC)
-
-    if args.verbose:
-        print("DDALPA found {} communities".format(len(ddC)))
-
-    if len(ddC) != 1:
-        # node in community network ---> community in ddC (A)
-        mpng = {lv2_node: com for com, lv2_node in enumerate(sorted(ddC.communities.keys()))}
-        # community in ddC (A) ---> node in community network
-        mpng_inv = {com: lv2_node for com, lv2_node in enumerate(sorted(ddC.communities.keys()))}
-            
-        A_C = community_network(G.A, ddC.dict_renamed)
-        G_C = Graph(A_C, 
-                    G.m, 
-                    A_C.shape[1], 
-                    np.array(A_C.sum(axis=1), dtype=float).reshape(-1,).tolist()
-                    )
-        odC = Labels(xrange(G_C.n), G_C.k)
-        dalpa(G_C, odC, True)
-        odalpaQ = modularity.modularity(G_C.A, G_C.k, G_C.m, odC)
-
-        if args.verbose:
-            print("ODALPA on community network revealed {} communities".format(len(odC)))
-
-        if odalpaQ >= ddQ: #and len(odC) != 1:
-            # We wish to regroup the nodes into the partitioning determined
-            # by offensive dalpa on the community network.
-            
-            n2c_mpng = [0] * G.n
-            for o_c, o_nodes in odC:
-                # community in community network, 
-                # nodes(communities in the original network) of this community
-                for o_node in o_nodes:
-                    # for each of these communities(nodes)
-                    for node in ddC.communities[mpng_inv[o_node]]:
-                        #map every node in the community o_node to o_c
-                        n2c_mpng[node] = o_c
-
-            ddC = Labels(n2c_mpng, G.k)
-            ddQ = odalpaQ
-    else:
-        # This is a dummy, so that we can jump into the next if 
-        # that takes us to bdpa!
-        odC = [0]
-
-    if len(odC) == 1:
-        # Starting over, really.
-        ddC = Labels(xrange(G.n), G.k)
-        bdpa(G, ddC)
-    else:
-        print("We are recursing")
-
-        largest_subset = list(ddC[ddC.largest[0]])
-        largest_subset.sort()
-        node_mpng = {i: j for i, j in enumerate(largest_subset)}
-        
-        A_sub = G.A[largest_subset, :][:, largest_subset] # This is probably costly
-
-        H = Graph(A_sub, A_sub.sum()*0.5, A_sub.shape[1],
-                np.array(A_sub.sum(axis=1), dtype=float).reshape(-1, ).tolist()
-                )
-        D = dpa(H, args)
-        C = copy.deepcopy(ddC)
-        for com, nodes in D:
-            C.insert_community([node_mpng[node] for node in nodes], G.k)
-        new_mod = modularity.modularity(G.A, G.k, G.m, C)
-        if  new_mod > ddQ:
-            ddC = C
-
-    return ddC
-
 def dalpa(G, C, offensive=False):
     """
-    Defensive/Offensive label propagation. 
+    Defensive and offensive diffusion and label propagation algorithm.
+
+    Propagates labels along the graph until an equilibrium is reached. 
 
     """
+    print("Running Dalpa. Offensive={}".format(offensive))
+    global MAX_ITER
     A, m, n, k = G
-    # initialize some variables
+    num_iter = 0
     delta = 0.0
-    num_iter = 1
-    num_moves = 1
-    # while not convergence
-    while num_moves > 0:
+    while num_iter < MAX_ITER:
         num_moves = 0
 
-        for i in fns.yield_random_modulo(n):
-
-            c_i = C.nodes[i]
+        for i in fns.yield_random_modulo(G.n):
             indices = A.indices[A.indptr[i]:A.indptr[i+1]]
-            data = A.data[A.indptr[i]:A.indptr[i+1]]
+            scores = defaultdict(float)
+            for j in indices:
+                if j != i:
+                    if not offensive:
+                        first_term = C.p[j]
+                    else:
+                        first_term = (1 - C.p[j])
+                    scores[C.nodes[j]] += first_term * (1.0 - delta * C.d[j])
 
-            neighbor_coms = defaultdict(float)
-            max_neighbor = (-1, -1.0)
-            com_edges = defaultdict(float)
-            d_dict = defaultdict(lambda: sys.maxint)
-            p_dict = defaultdict(float)
-            nb_dict = {}
+            old = C.nodes[i]
 
-            for ind, j in enumerate(indices):
-                aij = data[ind]
-                nb_dict[j] = aij
-                c_j = C.nodes[j]
-                p_j = C.p[j]
+            if scores:
+                
+                new = max(scores.iteritems(), key=itemgetter(1))[0]
 
-                if i == j:
-                    continue
+                if scores[old] < scores[new]:
+                    C.move(i, new, k[i])
 
-                d = C.d[j]
-                if d < d_dict[c_j]:
-                    d_dict[c_j] = d
+                    dist = n * 10
+                    C.p[i] = 0.0
+                    C.internal[i] = 0
 
-                if not offensive:
-                    neighbor_coms[c_j] += p_j*(1 - delta*d)*aij
-                    if C.internal[j] != 0:
-                        p_dict[c_j] += p_j/C.internal[j]
-                else:
-                    neighbor_coms[c_j] += (1 - p_j)*(1 - delta*d)*aij
-                    p_dict[c_j] += p_j/k[j]
+                    
+                    for v in A.indices[A.indptr[i]:A.indptr[i+1]]:
+                        if v != i:
+                            if C.nodes[v] == new:
+                                if C.d[v] < dist:
+                                    dist = C.d[v]
+                                C.internal[i] += 1
+                                C.internal[v] += 1
+                                if not offensive:
+                                    C.p[i] += C.p[v] / C.internal[v]
+                                else:
+                                    C.p[i] += C.p[v] / k[v]
 
-                if neighbor_coms[c_j] > max_neighbor[1]:
-                    max_neighbor = (c_j, neighbor_coms[c_j])
+                            elif C.nodes[v] == old:
+                                C.internal[v] -= 1
 
-                com_edges[c_j] += aij
-
-            # Move to the best community
-            best_match, best_val = max_neighbor
-            if best_match == -1:
-                # print("Best match = -1... ")
-                # print i
-                # print indices
-                # print data
-                # print A.indices
-                # print A.data
-                # print C.communities[C.nodes[i]]
-                continue
-
-            if best_match != c_i and best_val > neighbor_coms[c_i]:
-
-                C.move(i, best_match, k[i], com_edges[best_match], nb_dict)
-                C.d[i] = d_dict[best_match] + 1
-
-                if not (offensive and num_iter <= 1):                
-                    C.p[i] = p_dict[best_match]
-                num_moves += 1
-
-        num_iter += 1
-        delta = float(num_moves) / n
-        if delta >= 0.5:
+                    C.d[i] = dist + 1
+                    num_moves += 1
+        
+        ratio = num_moves / n
+        if ratio < 0.5 and num_iter > 0:
+            delta = ratio
+        else:
             delta = 0.0
 
-def bdpa_modified(G, C):
-    coms = C.dict_renamed
-    for c, nodes in coms.iteritems():
-        p_list = [C.p[j] for j in nodes]
-        for i in C[c].copy():
-            if C.p[i] <= np.median(p_list):
-                C.move(i, -1, G.k[i])
-                C.d[i] = 0
-                C.p[i] = 0
-    dalpa(G, C, True)
+        num_iter += 1
+        print num_moves
+        if num_moves == 0:
+            break
+
+    if num_iter >= MAX_ITER:
+        print "reached max iter"
+    print "found {} communities".format(len(C))
+
+def dpa(G, args):
+    """
+    Diffusion and propagation algorithm. 
+    
+    """
+    # Run defensive dalpa
+    defensive_C = Labels(xrange(G.n), G.k, G.A.diagonal())
+    dalpa(G, defensive_C, offensive=False)
+    defensive_Q = modularity.modularity(G.A, G.k, G.m, defensive_C)
+
+    # Construct the community network
+    A_C = community_network(G.A, defensive_C.dict_renamed)
+    G_C = Graph(A_C, 
+                G.m,
+                A_C.shape[1], 
+                np.array(A_C.sum(axis=1), dtype=float).reshape(-1,).tolist()
+                )
+
+    # Run offensive dalpa on the community network
+    offensive_C = Labels(xrange(G_C.n), G_C.k, G_C.A.diagonal())
+    dalpa(G_C, offensive_C, offensive=True)
+    offensive_Q = modularity.modularity(G_C.A, G_C.k, G_C.m, offensive_C)
+
+    # if the modularity of the offensive run is higher than the modularity
+    # of the defensive run, we wish to transfer the labels/communities of
+    # community network onto the real network. 
+
+    if offensive_Q > defensive_Q and len(offensive_C) > 1:
+        clustering = [-1] * G.n
+        # community in G_C holds communities in G
+        for high_level_c, low_level_cs in offensive_C:
+            for low_level_c in low_level_cs:
+                for node in defensive_C[low_level_c]:
+                    clustering[node] = high_level_c
+     
+        # Keep in mind that the internal edges are wrong below
+        defensive_C = Labels(clustering, G.k, G.A.diagonal())
+        defensive_Q = offensive_Q
+
+        # # If we need to fix the intra-community edges:
+        # defensive_C.internal = [0] * G.n
+        # for c, nodes in defensive_C:
+        #     for u in nodes:
+        #         for v in G.A.indices[G.A.indptr[u]:G.A.indptr[u+1]]:
+        #             if v != u:
+        #                 if defensive_C.nodes[v] == c:
+        #                     defensive_C.internal[u] += 1
+    
+    if len(offensive_C) == 1:
+        defensive_C = Labels(xrange(G.n), G.k, G.A.diagonal())
+        defensive_C = bdpa(G, defensive_C)
+        return defensive_C
+    else:
+        print "RECURSING"
+        print G.m
+        # print defensive_C.dict
+        largest = list(defensive_C[defensive_C.largest])
+        largest.sort()
+
+        # mapping vertex in subset-graph to vertex in real graph
+        mapping = {i: j for i, j in enumerate(largest)}
+
+        A_subset = G.A[largest, :][:, largest]
+        G_subset = Graph(
+            A_subset,
+            A_subset.sum() / 2,
+            A_subset.shape[1],
+            np.array(A_subset.sum(axis=1), dtype=float).reshape(-1, ).tolist()
+            )
+        recursive_C = dpa(G_subset, args)
+        new_C = copy.deepcopy(defensive_C)
+
+        for c, nodes in recursive_C:
+            new_C.insert_community([mapping[node] for node in nodes], G.k)
+
+        new_Q = modularity.modularity(G.A, G.k, G.m, new_C)
+
+        if new_Q > defensive_Q:
+            defensive_C = new_C
+
+        return new_C
 
 def bdpa(G, C):
-    dalpa(G, C, False)
-    bdpa_modified(G, C)
+    print "Running BDPA"
+    dalpa(G, C, offensive=False)
+    defensive_Q = modularity.modularity(G.A, G.k, G.m, C)
+    print "defensive_Q = {} ".format(defensive_Q)
+    new_C = copy.deepcopy(C)
+    for c, nodes in C:
+        median = np.median([C.p[j] for j in nodes])
+        for i in C[c]:
+            if new_C.p[i] <= median:
+                new_C.move(i, -1, G.k[i])
+                new_C.d[i] = 0
+                new_C.p[i] = 0
+                new_C.internal[i] = G.A[i,i]
+    # Fix internal edges
+    new_C.internal = [0] * G.n
+    for i in xrange(G.n):
+        for j in G.A.indices[G.A.indptr[i]:G.A.indptr[i+1]]:
+            if new_C.nodes[i] == new_C.nodes[j]:
+                new_C.internal[i] += 1
+    dalpa(G, new_C, True)
+    offensive_Q = modularity.modularity(G.A, G.k, G.m, new_C)
+    print "offensive_Q = {} ".format(offensive_Q)
+    if offensive_Q > defensive_Q:
+        return new_C
+    else:
+        return C
+
+
+
--- labels.py
+++ labels.py
@@ -2,32 +2,9 @@ from communities import Communities
 
 class Labels(Communities):
 
-    def __init__(self, iterable, k):
+    def __init__(self, iterable, k, diagonal):
         super(Labels, self).__init__(iterable, k)
-        self.internal = [sum(k[i] for i in self[j]) for j in self.dict.keys()]
+        self.internal = [diagonal[i] for i in iterable]
+        # self.internal = [0.0] * len(self.nodes)
         self.d = [0.0] * len(self.nodes)
         self.p = [1.0/len(self.nodes)] * len(self.nodes)
-
-    def move(self, i, s, k_i, intra=None, nb_dict=None):
-        old = self.nodes[i]
-        super(Labels, self).move(i, s, k_i)
-        if intra is not None:
-            try:
-                self.internal[i] = intra
-            except IndexError:
-                'Warning, IndexError: No such vertex in list.'
-        if nb_dict is not None:
-            for j, aij in nb_dict.iteritems():
-                if self.nodes[j] == old:
-                    self.internal[j] -= aij
-                elif self.nodes[j] == self.nodes[i]:
-                    self.internal[j] += aij
-
-    def recluster(self, com_dict, k):
-        super(Labels, self).recluster(com_dict, k)
-        # Should avoid this iteration
-        for name, coms in com_dict.iteritems():
-            for c_i in coms[1:]:
-                for i in self[c_i]:
-                    # WILD approximation
-                    self._internal[i] = k - self._internal[i]
